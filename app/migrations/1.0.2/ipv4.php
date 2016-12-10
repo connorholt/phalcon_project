3b39c64afd3553bebcc93ea52beb5c6b9ac28f66
dmitriy <dmitriy>
2016-12-10 23:31:01 +0300

    Индекс для поиска по базе ip адресов
    Функциональный индекс, использующий расширение ip4r для поиска ip адреса в интервале

---
+++ app/migrations/1.0.2/ipv4.php
@@ -0,0 +1,144 @@
+<?php 
+
+use Phalcon\Db\Column;
+use Phalcon\Db\Index;
+use Phalcon\Db\Reference;
+use Phalcon\Mvc\Model\Migration;
+
+/**
+ * Class Ipv4Migration_102
+ */
+class Ipv4Migration_102 extends Migration
+{
+    /**
+     * Define the table structure
+     *
+     * @return void
+     */
+    public function morph()
+    {
+        $this->morphTable('ipv4', [
+                'columns' => [
+                    new Column(
+                        'id',
+                        [
+                            'type' => Column::TYPE_BIGINTEGER,
+                            'notNull' => true,
+                            'autoIncrement' => true,
+                            'first' => true
+                        ]
+                    ), 
+                    new Column(
+                        'ip_from',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 1,
+                            'after' => 'id'
+                        ]
+                    ),
+                    new Column(
+                        'ip_to',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'size' => 1,
+                            'after' => 'ip_from'
+                        ]
+                    ),
+                    new Column(
+                        'country_code',
+                        [
+                            'type' => Column::TYPE_CHAR,
+                            'notNull' => true,
+                            'size' => 2,
+                            'after' => 'ip_to'
+                        ]
+                    ),
+                    new Column(
+                        'country_name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 64,
+                            'after' => 'country_code'
+                        ]
+                    ),
+                    new Column(
+                        'region_name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 128,
+                            'after' => 'country_name'
+                        ]
+                    ),
+                    new Column(
+                        'city_name',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 128,
+                            'after' => 'region_name'
+                        ]
+                    ),
+                    new Column(
+                        'latitude',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'city_name'
+                        ]
+                    ),
+                    new Column(
+                        'longitude',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 1,
+                            'after' => 'latitude'
+                        ]
+                    ),
+                    new Column(
+                        'zip_code',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 30,
+                            'after' => 'longitude'
+                        ]
+                    ),
+                    new Column(
+                        'time_zone',
+                        [
+                            'type' => Column::TYPE_VARCHAR,
+                            'notNull' => true,
+                            'size' => 8,
+                            'after' => 'zip_code'
+                        ]
+                    )
+                ],
+            ]
+        );
+    }
+
+    /**
+     * Run the migrations
+     *
+     * @return void
+     */
+    public function up()
+    {
+        self::$_connection->execute('CREATE INDEX IF NOT EXISTS ipv4_ip4r_idx ON ipv4 USING gist (ip4r(ip_from, ip_to))');
+    }
+
+    /**
+     * Reverse the migrations
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+
+}
